1f4f0f8a38fc90b2ee6318d4ca8e40bc791ec67d
Yuussef <chahbiyuussef@gmail.com>
2023-09-19 17:08:33 +0200

    add architecture

---
+++ src/main/java/com/sijo/Dossiercompback/DossierCompBackApplication.java
@@ -0,0 +1,13 @@
+package com.sijo.Dossiercompback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossierCompBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DossierCompBackApplication.class, args);
+	}
+
+}
