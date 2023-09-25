ae8b9af35a9c8497dc3cf1ce76c269a6d6d5df7d
Yuussef <chahbiyuussef@gmail.com>
2023-09-25 11:50:02 +0200

    add features to word

--- src/main/java/com/sijo/Dossiercompback/controller/DocumentCompet.java
+++ src/main/java/com/sijo/Dossiercompback/controller/DocumentCompet.java
@@ -4,17 +4,11 @@ import com.sijo.Dossiercompback.service.JasperReportService;
 import jakarta.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
+import org.springframework.web.bind.annotation.*;
+
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 
 @RestController
 @RequestMapping("/api/reports")
@@ -36,11 +30,11 @@ public class DocumentCompet {
                 response.getOutputStream().flush();
         }
 
-        @GetMapping(value = "/download")
-        ResponseEntity<Void> exportDynamicTransactionReport(
+        @CrossOrigin
+        @GetMapping(value = "/word")
+        public void exportDynamicTransactionReport(
                                                             HttpServletResponse response) throws IOException, JRException{
                 jasperReportService.generateWordFromJasperReport( response);
-    return ResponseEntity.ok().build();
 }
 
 
--- src/main/java/com/sijo/Dossiercompback/service/JasperReportService.java
+++ src/main/java/com/sijo/Dossiercompback/service/JasperReportService.java
@@ -1,26 +1,17 @@
 package com.sijo.Dossiercompback.service;
 
+import com.sijo.Dossiercompback.model.SampleBean;
 import jakarta.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.data.JsonDataSource;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
-import net.sf.jasperreports.engine.export.JRRtfExporter;
 import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
-import net.sf.jasperreports.engine.*;
-import net.sf.jasperreports.engine.export.JRPdfExporter;
-import org.springframework.stereotype.Service;
-
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class JasperReportService {
@@ -47,22 +38,33 @@ public class JasperReportService {
     }
 
     public void generateWordFromJasperReport(HttpServletResponse response) throws JRException, IOException {
-        // Load the JasperReport template (.jrxml)
+        List<SampleBean> dataList = new ArrayList<SampleBean>();
         JasperReport jasperReport = JasperCompileManager.compileReport(getClass().getResourceAsStream("/documentComp.jrxml"));
 
         // Create a data source (you can replace this with your data source)
         JRDataSource dataSource = createYourDataSource();
-
-        // Fill the report with data
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, dataSource);
-
+        SampleBean sampleBean = new SampleBean();
+        sampleBean.setName("some name");
+        sampleBean.setColor("green");
+        sampleBean.setJob("Fullstack developer");
+        sampleBean.setExperience("5 ans experience");
+        sampleBean.setAnnee("2018");
+        sampleBean.setTitrediplome("Ingénieur d'état en Télécoms et Réseaux ");
+        sampleBean.setEcole("– Ecole Nationale des Sciences Appliquées");
+        sampleBean.setAnnee2("2013");
+        sampleBean.setTitrediplome2("Baccalauréat en Sciences Mathématique");
+        sampleBean.setEcole2("– Lycée XX");
+        dataList.add(sampleBean);
+        JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(dataList);
+        Map<String, Object> parameters = new HashMap();
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, beanColDataSource);
         JRDocxExporter exporter = new JRDocxExporter();
         exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
         exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream()));
-        response.setHeader(
-                HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + "fileName" + ".docx");
+        response.setHeader("Content-Disposition", "attachment;filename=jasperfile.docx");
         response.setContentType("application/octet-stream");
         exporter.exportReport();
+
     }
 
     private JRDataSource createYourDataSource() {
